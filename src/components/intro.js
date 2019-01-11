911e9f86b40d3e5a66da18db4c0b31399ac4341f
unknown <gjbonner@gmail.com>
2019-01-11 14:04:17 -0500

    intro-done

--- src/components/intro.js
+++ src/components/intro.js
@@ -2,16 +2,16 @@ import React, { Component } from 'react'
 import '../css/intro.css'
 import '../css/animate.css'
 const Intro = () => {
+
+  const HandleClick = () => {
+    console.log('clicked')
+  }
   return(
     <div>
       <div className='backgroundImg'>
-        <div>
-        <div className='animated bounceInUp'>
-          <h1 className='hello'>Hi, I'm Giles Bonner.</h1>
-        </div>
-        <div className='animated bounceInUp delay-0.2s'>
-          <h1 className='enter'>sup</h1>
-        </div>
+        <div className='introContainer animated bounceInUp'>
+          <h1 className='hello'>Hi, I'm Giles Bonner</h1>
+          <h3 className='align'><button onClick={HandleClick} className='enter'>enter</button></h3>
         </div>
       </div>
     </div>
