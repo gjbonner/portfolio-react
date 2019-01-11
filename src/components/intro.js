97c4049d03d72bb74632a9798da828815816f0f7
unknown <gjbonner@gmail.com>
2019-01-11 11:52:46 -0500

    introComponent

---
+++ src/components/intro.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import '../css/intro.css'
+const Intro = () => {
+  return(
+    <div>
+      <div className='backgroundImg'>
+        
+      </div>
+    </div>
+  )
+}
+
+export default Intro
