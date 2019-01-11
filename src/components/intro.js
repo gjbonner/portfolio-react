dd0f504b515736d1e3e9f0e9e24d13a6dff4a777
unknown <gjbonner@gmail.com>
2019-01-11 12:31:11 -0500

    introAnimation

--- src/components/intro.js
+++ src/components/intro.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react'
 import '../css/intro.css'
+import '../css/animate.css'
 const Intro = () => {
   return(
     <div>
       <div className='backgroundImg'>
-        
+        <div>
+        <div className='animated bounceInUp'>
+          <h1 className='hello'>Hi, I'm Giles Bonner.</h1>
+        </div>
+        <div className='animated bounceInUp delay-0.2s'>
+          <h1 className='enter'>sup</h1>
+        </div>
+        </div>
       </div>
     </div>
   )
