daabd05cd2668ed557b508a7080009a30f9979e5
unknown <gjbonner@gmail.com>
2019-01-13 12:24:56 -0500

    wrapper

--- src/containers/home.js
+++ src/containers/home.js
@@ -2,12 +2,17 @@ import React, { Component } from 'react'
 import Header from '../components/navbar'
 import PortfolioCard from '../components/portfolioCard'
 import About from '../components/about'
+import '../css/home.css'
 const PortfolioContainer = () => {
   return(
     <div>
-      <Header />
+    <Header />
+    <div className='wrapper'>
       <About />
+    </div>
+    <div className='wrapper'>
       <PortfolioCard />
+    </div>
     </div>
   )
 }
