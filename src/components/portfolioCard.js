1d1566b312734537cccfdb30bdcd033627a5f7d6
unknown <gjbonner@gmail.com>
2019-01-16 11:34:41 -0500

    flip cards working

--- src/components/portfolioCard.js
+++ src/components/portfolioCard.js
@@ -5,24 +5,72 @@ const PortfolioCards = () => {
     <div>
       <h1>Portfolio</h1>
     <div className='cards'>
-    <div className='card'>
-      <img className='cardImg' src='https://res.cloudinary.com/dtdt6vcwa/image/upload/v1547654275/whatToMake.png'/>
-    </div>
-    <div className='card'>
-      <img src='https://res.cloudinary.com/dtdt6vcwa/image/upload/v1547572156/imageedit_3_3205548450.png'/>
-    </div>
-    <div className='card'>
-      <img src='https://res.cloudinary.com/dtdt6vcwa/image/upload/v1547572156/imageedit_3_3205548450.png'/>
-    </div>
-    <div className='card'>
-      <img src='https://res.cloudinary.com/dtdt6vcwa/image/upload/v1547572156/imageedit_3_3205548450.png'/>
-    </div>
-    <div className='card'>
-      <img src='https://res.cloudinary.com/dtdt6vcwa/image/upload/v1547572156/imageedit_3_3205548450.png'/>
-    </div>
-    <div className='card'>
-      <img src='https://res.cloudinary.com/dtdt6vcwa/image/upload/v1547572156/imageedit_3_3205548450.png'/>
-    </div>
+      <div className='card'>
+        <div className='cardInner'>
+          <div className='cardFront'>
+            <img className='cardImg' src='https://res.cloudinary.com/dtdt6vcwa/image/upload/v1547654275/whatToMake.png'/>
+            <p className='appName'>What To Make</p>
+          </div>
+          <div className='cardBack'>
+            <p>back of card content here</p>
+          </div>
+        </div>
+      </div>
+      <div className='card'>
+        <div className='cardInner'>
+          <div className='cardFront'>
+            <img className='cardImg' src='https://res.cloudinary.com/dtdt6vcwa/image/upload/v1547654275/whatToMake.png'/>
+            <p className='appName'>What To Make</p>
+          </div>
+          <div className='cardBack'>
+            <p>back of card content here</p>
+          </div>
+        </div>
+      </div>
+      <div className='card'>
+        <div className='cardInner'>
+          <div className='cardFront'>
+            <img className='cardImg' src='https://res.cloudinary.com/dtdt6vcwa/image/upload/v1547654275/whatToMake.png'/>
+            <p className='appName'>What To Make</p>
+          </div>
+          <div className='cardBack'>
+            <p>back of card content here</p>
+          </div>
+        </div>
+      </div>
+      <div className='card'>
+        <div className='cardInner'>
+          <div className='cardFront'>
+            <img className='cardImg' src='https://res.cloudinary.com/dtdt6vcwa/image/upload/v1547654275/whatToMake.png'/>
+            <p className='appName'>What To Make</p>
+          </div>
+          <div className='cardBack'>
+            <p>back of card content here</p>
+          </div>
+        </div>
+      </div>
+      <div className='card'>
+        <div className='cardInner'>
+          <div className='cardFront'>
+            <img className='cardImg' src='https://res.cloudinary.com/dtdt6vcwa/image/upload/v1547654275/whatToMake.png'/>
+            <p className='appName'>What To Make</p>
+          </div>
+          <div className='cardBack'>
+            <p>back of card content here</p>
+          </div>
+        </div>
+      </div>
+      <div className='card'>
+        <div className='cardInner'>
+          <div className='cardFront'>
+            <img className='cardImg' src='https://res.cloudinary.com/dtdt6vcwa/image/upload/v1547654275/whatToMake.png'/>
+            <p className='appName'>What To Make</p>
+          </div>
+          <div className='cardBack'>
+            <p>back of card content here</p>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
   )
