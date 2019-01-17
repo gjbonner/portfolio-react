b88f8f04cb02fb2cf54eb5f690a66f1cca988dc1
unknown <gjbonner@gmail.com>
2019-01-17 11:02:33 -0500

    typedjs

--- src/components/about.js
+++ src/components/about.js
@@ -1,23 +1,34 @@
-import React from 'react'
+import React, { Component } from 'react'
 import '../css/about.css'
-const About = () => {
-  return(
-    <div>
-      <img id='robot' src='https://res.cloudinary.com/dtdt6vcwa/image/upload/v1547677073/robot.png' alt='robot' />
-      <h1>About Me</h1>
-      <p>I’m a Full Stack developer with a passion for building easy to use, state of the art websites and applications implementing
-       clean and efficient code. With experience in Ruby on Rails, JavaScript, and React and a background in data analytics,
-       I discovered web development through my enthusiasm for technology, curiosity of how things work,
-       and my love for the idea of being given a blank canvas, a problem and countless ways to solve it!
-     I bring strong skills in collaborative and agile development that help team-driven,
-     tightly knit companies efficiently develop current and fit for market solutions.<br/><br/>
+import * as Typed from 'typed.js'
+class About extends Component {
+  componentDidMount(){
+    const typed = new Typed('#typed', {
+      strings: ["Hi there, I'm Giles Bonner."],
+      typeSpeed: 50,
+      startDelay: 500
+    })
+  }
+  render(){
+    return(
+      <div>
+        <h1><span id='typed'></span></h1>
+        <img id='robot' src='https://res.cloudinary.com/dtdt6vcwa/image/upload/v1547677073/robot.png' alt='robot' />
+        <h1>About Me</h1>
+        <p>I’m a Full Stack developer with a passion for building easy to use, state of the art websites and applications implementing
+         clean and efficient code. With experience in Ruby on Rails, JavaScript, and React and a background in data analytics,
+         I discovered web development through my enthusiasm for technology, curiosity of how things work,
+         and my love for the idea of being given a blank canvas, a problem and countless ways to solve it!
+       I bring strong skills in collaborative and agile development that help team-driven,
+       tightly knit companies efficiently develop current and fit for market solutions.<br/><br/>
 
-     I love to cook and have even built a food oriented app which you can see in my portfolio! It is now obvious why it suits me
-     so well! It is a deconstruction of a problem and engineering a solution to the problem to get a result. Another big interest of mine
-     is building computers and playing around with IoT.
-     </p>
-    </div>
-  )
+       I love to cook and have even built a food oriented app which you can see in my portfolio! It is now obvious why it suits me
+       so well! It is a deconstruction of a problem and engineering a solution to the problem to get a result. Another big interest of mine
+       is building computers and playing around with IoT.
+       </p>
+      </div>
+    )
+  }
 }
 
 export default About
