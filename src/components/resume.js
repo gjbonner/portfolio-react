69fed44ea9c0c780665c2ecc305721029d45e37c
unknown <gjbonner@gmail.com>
2019-01-30 17:17:30 -0500

    resume

--- src/components/resume.js
+++ src/components/resume.js
@@ -5,7 +5,7 @@ const Resume = () => {
     <div>
     <div className='wrapper'>
       <div>
-        <img className='img' alt='resume' src='https://res.cloudinary.com/dtdt6vcwa/image/upload/v1548886394/Giles_Bonner_Resume.pdf'/>
+        <img className='img' alt='resume' src='https://res.cloudinary.com/dtdt6vcwa/image/upload/v1548886589/Giles_Bonner_Resume-1.jpg'/>
       </div>
     </div>
     </div>
