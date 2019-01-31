4b583ae9183d063c943b5e1aaffa20893acb0054
unknown <gjbonner@gmail.com>
2019-01-31 10:07:47 -0500

    wrapper mobile fix

--- src/components/resume.js
+++ src/components/resume.js
@@ -2,13 +2,9 @@ import React from 'react'
 import '../css/resume.css'
 const Resume = () => {
   return(
-    <div>
-    <div className='wrapper'>
-      <div>
-        <img className='img' alt='resume' src='https://res.cloudinary.com/dtdt6vcwa/image/upload/v1548886589/Giles_Bonner_Resume-1.jpg'/>
+      <div className='wrapper'>
+        <img alt='resume' src='https://res.cloudinary.com/dtdt6vcwa/image/upload/v1548886589/Giles_Bonner_Resume-1.jpg' />
       </div>
-    </div>
-    </div>
   )
 }
 
