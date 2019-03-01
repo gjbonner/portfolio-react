03558cd916f595f408468b8b01cf6d7f35e8d54d
unknown <gjbonner@gmail.com>
2019-03-01 12:39:48 -0500

    resume

--- src/components/resume.js
+++ src/components/resume.js
@@ -3,7 +3,7 @@ import '../css/resume.css'
 const Resume = () => {
   return(
       <div className='resumeWrapper'>
-        <img alt='resume' src='https://res.cloudinary.com/dtdt6vcwa/image/upload/v1548948216/Giles_Bonner_Resume.jpg' />
+        <img alt='resume' src='https://res.cloudinary.com/dtdt6vcwa/image/upload/v1551461907/resume.jpg' />
       </div>
   )
 }
