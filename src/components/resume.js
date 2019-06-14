7a5ecc13e8b8d9c8a80bc4b861a8b4e109fabd86
Giles Bonner <gjbonner@gmail.com>
2019-06-14 11:44:10 -0400

    Update resume.js

--- src/components/resume.js
+++ src/components/resume.js
@@ -3,7 +3,7 @@ import '../css/resume.css'
 const Resume = () => {
   return(
       <div className='resumeWrapper'>
-        <img alt='resume' src='https://res.cloudinary.com/dtdt6vcwa/image/upload/v1557330175/Giles_Bonner_Resume-1.jpg'/>
+        <img alt='resume' src='https://res.cloudinary.com/dtdt6vcwa/image/upload/v1560526790/Giles_Bonner_Resume-1.jpg'/>
       </div>
   )
 }
